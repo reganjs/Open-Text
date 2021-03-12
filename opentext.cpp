5c29804ffe737c29c62b3a36f39966dfec240fd3
Regan <52940978+reganjs@users.noreply.github.com>
2021-03-11 20:44:16 -0600

    Update and rename TMA1Question2.cpp to opentext.cpp

--- opentext.cpp
+++ opentext.cpp
@@ -1,6 +1,4 @@
-//Regan Setter
-//Student ID 3505894
-//Computer Science 206
+//Regan 
 //January 5th, 2021
 
 //Write a program that opens a (text) file, and displays the contents of that file one line at a time. After a line has been displayed, the program should wait for the user to press the key before displaying the next line in the file.
